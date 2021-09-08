56f8694c6ca4b36ce142782ebffb429afee232db
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-09-08 13:08:59 -0300

    nav en el home, paque pg y js de consultas para la base de datos e importación del paquete bootstrap

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const express=require('express')
 const app=express()
 
 
-
+//
 app.listen("3000",()=>console.log('Puerto 3000 encendido'))
+
+app.use(express.static("public"))
+app.use("/css",express.static(__dirname+"/node_modules/bootstrap/dist/css"))
+/* app.use("/css", express.static(__dirname + "/css")) */
+
+//rutas
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/home.html")
+})
